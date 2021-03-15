360b180bf046d050fab0e07ae3cfd0924e478d7f
bill-hx-liu <37031408+bill-hx-liu@users.noreply.github.com>
2021-03-15 19:20:40 +0800

    day02 搭建环境和医院设置

---
+++ common/common_util/src/main/java/com/atguigu/yygh/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.atguigu.yygh.common.exception;
+
+/**
+ * @author:lhx
+ * @date 2021/3/15 18:49
+ * @function:
+ */
+
+import com.atguigu.yygh.common.result.Result;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 全局异常处理类
+ *
+ */
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)//指定参数为所有的异常的集合
+    @ResponseBody//结果以json输出
+    public Result error(Exception e){
+        e.printStackTrace();
+        return Result.fail();
+    }
+
+    /**
+     * 自定义异常处理方法
+     * @param e
+     * @return
+     */
+    @ExceptionHandler(YyghException.class)
+    @ResponseBody
+    public Result error(YyghException e){
+        return Result.build(e.getCode(), e.getMessage());
+    }
+}
