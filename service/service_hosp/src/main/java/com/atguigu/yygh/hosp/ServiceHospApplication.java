add258cd4f7a0c4731bdf2c3fb34d343c2862626
bill-hx-liu <37031408+bill-hx-liu@users.noreply.github.com>
2021-03-19 09:37:53 +0800

    day05 医院设置模块无法登录

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -1,20 +1,18 @@
 package com.atguigu.yygh.hosp;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.stereotype.Component;
 
-/**
- * @author:lhx
- * @date 2021/3/15 15:33
- * @function:
- */
 @SpringBootApplication
 @ComponentScan(basePackages = "com.atguigu")
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.atguigu")
 public class ServiceHospApplication {
     public static void main(String[] args) {
         SpringApplication.run(ServiceHospApplication.class, args);
     }
 }
-
